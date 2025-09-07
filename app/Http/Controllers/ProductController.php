85025cf808db75302cab627a399f5a4d4841f396
Tedy wibisono <tedywibi2932@gmaill.com>
2025-09-07 12:17:44 +0700

    fixing bug

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -235,7 +235,7 @@ class ProductController extends Controller
         ->leftJoin('tabel_detail_kondisi as korosi_kondisi', 'tabel_produk.korosi', '=', 'korosi_kondisi.id')
         ->leftJoin('tabel_detail_kondisi as expired_kondisi', 'tabel_produk.expired', '=', 'expired_kondisi.id')
         ->leftJoin('tabel_gedung as lokasi_name', 'tabel_produk.lokasi', '=', 'lokasi_name.id')
-        ->leftJoin('tabel_titik_penempatan as placement_point', 'tabel_produk.titik_penempatan_id', '=', 'tabel_titik_penempatan.id')
+        ->leftJoin('tabel_titik_penempatan as placement_point', 'tabel_produk.titik_penempatan_id', '=', 'placement_point.id')
         ->where('kode_barang', $request->id_barang)
         ->first();
 
